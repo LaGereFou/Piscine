d19fc8736df9ac8aaa01cf09ba6a5afb0477ba31
Jean-Baptiste SCELLES <jean-baptiste.scelles@edu.ece.fr>
2018-04-04 13:05:23 +0200

    nouveau commit

--- main.cpp
+++ main.cpp
@@ -7,23 +7,35 @@
 #include <stdio.h>
 #include <math.h>
 
+
 using namespace std;
+
 void graphe_1();
-void menu(BITMAP*buffer,BITMAP*menus,BITMAP*collisionmenus);
+void menu();
+void init_allegro();
+
+
+
 int main()
 {
-    //declaration des variables
-    int s,h,d;// Coordonnées mouse et variable play musique
-    BITMAP*menus;// Page menu
-    BITMAP*collisionmenus;// Page de collision menu
-    BITMAP*buffer;// Buffer
-    MIDI*music;// Musique du générique
-    int bleu,vert,rouge;// Couleurs pour getpixels
 
 
-    // Il y aura du hasard
-    srand(time(NULL));
+    //initialisation allegro
+    init_allegro();
 
+    /** Il y aura du hasard
+    srand(time(NULL));**/
+
+    menu();// Appel du menu
+
+
+    return 0;
+
+}
+END_OF_MAIN();
+
+void init_allegro()
+{
     // Lancer allegro et le mode graphique
     allegro_init();
     install_keyboard();
@@ -32,10 +44,10 @@ int main()
     install_sound(DIGI_AUTODETECT,MIDI_AUTODETECT,NULL);
 
     // Lancer une fonction temps
-   /* LOCK_VARIABLE(timer_s);
-    LOCK_VARIABLE(timer_m);
-    LOCK_FUNCTION(faire_timer);
-    install_int_ex(faire_timer, SECS_TO_TIMER(1));*/
+    /* LOCK_VARIABLE(timer_s);
+     LOCK_VARIABLE(timer_m);
+     LOCK_FUNCTION(faire_timer);
+     install_int_ex(faire_timer, SECS_TO_TIMER(1));*/
 
     set_color_depth(desktop_color_depth());
     if (set_gfx_mode(GFX_AUTODETECT_WINDOWED,800,600,0,0)!=0)
@@ -45,104 +57,155 @@ int main()
         exit(EXIT_FAILURE);
     }
 
+}
+
+/// MENU DU JEU
+void menu()
+{
+    int a,b,c=0, d;// coordonnées de souris et getpixel
+    int rouge = makecol(255, 0, 0), vert = makecol(0,255,0), bleu = makecol(0,0,255);
+    MIDI*music;// Musique du générique
+    //declaration des variables
+    BITMAP*menus;// Page menu
+    BITMAP*collisionmenus;// Page de collision menu
+    BITMAP*buffer;// Buffer
+
+
     //chargement image music
     menus=load_bitmap("Menu_image.bmp",NULL);
     if (!menus)
-        {
-            allegro_message("Problème menus");
-        }
+    {
+        allegro_message("Problème menus");
+    }
     collisionmenus=load_bitmap("Menu_collision.bmp",NULL);
     if (!collisionmenus)
-        {
-            allegro_message("Problème collisionmenus");
-        }
+    {
+        allegro_message("Problème collisionmenus");
+    }
 
     buffer=create_bitmap(SCREEN_W,SCREEN_H);
     if (!buffer)
-        {
-            allegro_message("Problème création bitmap");
-        }
+    {
+        allegro_message("Problème création bitmap");
+    }
 
 
     music=load_midi("Johnny.mid");
 
     if (!music)
-        {
-            allegro_message("Problème musique");
-        }
-    d=play_midi(music,1);
+    {
+        allegro_message("Problème musique");
+    }
+
+    d = play_midi(music,1);
 
     show_mouse(screen);
-    //chargement image
-    blit(menus,buffer,0,0,0,0,SCREEN_W,SCREEN_H);
-
-    rouge=makecol(255,0,0);
-    vert=makecol(0,255,0);
-    bleu=makecol(0,0,255);
 
 
-        menu(buffer,menus,collisionmenus);// Appel du menu
-
-
-
-
-    return 0;
-
-} END_OF_MAIN();
-
-/// MENU DU JEU
-void menu(BITMAP*buffer,BITMAP*menus,BITMAP*collisionmenus)
-{
-    int a,b,c=0;// coordonnées de souris et getpixel
-    int onetime = 0;
-
-
-    blit(menus,buffer,0,0,0,0,SCREEN_W,SCREEN_H);
-    if(mouse_b&1 )
+    while(!key[KEY_ESC])
     {
-        a=mouse_x;
-        b=mouse_y;
-        c=getpixel(collisionmenus,a,b);
+        blit(menus,buffer,0,0,0,0,SCREEN_W,SCREEN_H);
+        blit(buffer,screen,0,0,0,0,SCREEN_W,SCREEN_H);
+
+        //récupération des données de la souris
+        if(mouse_b&1)
+        {
+            b = mouse_x;
+            a = mouse_y;
+            c = getpixel(collisionmenus,a,b);
+        }
+
+
+        //choix du menus
+        if( c == rouge )
+        {
+            clear_bitmap(screen);
+            graphe_1();
+            c = makecol(0,0,0);
+        }
+        else if( c == vert)
+        {
+            std::cout << " Deuxieme graphe" << std::endl;
+            clear_bitmap(screen);
+        }
+        else if( c == vert)
+        {
+            std::cout << " troisieme graphe" << std::endl;
+            clear_bitmap(screen);
+        }
+
+
     }
 
-    //choix du menus
-
-
-    if( c == makecol(255,0,0) && onetime==0 )
-    {
-        clear_bitmap(buffer);
-        graphe_1();
-        onetime=1;
-    }
-    else if( c == makecol(0,255,0) && onetime==0 )
-    {
-        std::cout << " Deuxieme graphe" << std::endl;
-        onetime=1;
-    }
-    else if( c == makecol(0,0,255) && onetime==0)
-    {
-        std::cout << " troisieme graphe" << std::endl;
-        onetime=1;
-    }
-
-    blit(buffer,screen,0,0,0,0,SCREEN_W,SCREEN_H);
     clear_bitmap(buffer);
+
 
 
 }
 
 void graphe_1()
 {
-
-    std::vector<Aretes> test;
     Graphe a;
-    int cmp = 1;
     a.recuperation();
+    int x = 0, y = 0;
+    bool test = false;
+
+    BITMAP* buffer;
+    BITMAP* savane;
+
+    //initialisation des bitmaps
+    savane = load_bitmap("Images/Savane.bmp", NULL);
+    if(!savane)
+    {
+        allegro_message("Problème Savane");
+    }
+
+    buffer = create_bitmap(SCREEN_W, SCREEN_H);
+    if(!buffer)
+    {
+        allegro_message("Problème Buffer_Savane");
+
+    }
+
+    show_mouse(screen);
+
+    while( test != true && !key[KEY_ESC])
+    {
+        //Affichage fond de graphe
+        blit(savane, buffer, 0,0,0,0, SCREEN_W, SCREEN_H);
+        blit(buffer,screen, 0,0,0,0, SCREEN_W, SCREEN_H);
+
+
+        if(mouse_b&1)
+        {
+            x = mouse_x;
+            y = mouse_y;
+            std::cout << "x = "<< x <<std::endl;
+            std::cout << "y = "<< y <<std::endl;
+        }
+
+        if( x > 710 && y < 55)
+        {
+                        std::cout << "okk"<< std::endl;
+
+            test =  true;
+        }
+
+
+    }
+
+
+
+    //affichage graphe sur console
     std::cout << a.getnom()<< std::endl;
     std::cout <<   a.getnbaretes() << std::endl;
-    for (int i =0; i< a.getnbaretes(); i++)
+
+
+    for(int i = 0; i < a.getnbaretes(); i++)
     {
-        std::cout << " Voici l'arete " << cmp <<std::endl<<std::endl;
+
+
+        std::cout << " Voici l'arete " << i + 1 <<std::endl<<std::endl;
         std::cout <<   a.getaretes()[i].gets1().getnom()<< "  ";
         std::cout <<   a.getaretes()[i].gets1().getx()<< "  ";
         std::cout <<   a.getaretes()[i].gets1().gety()<< std::endl;
@@ -150,9 +213,8 @@ void graphe_1()
         std::cout <<   a.getaretes()[i].gets2().getx()<< "  ";
         std::cout <<   a.getaretes()[i].gets2().gety()<< std::endl<<std::endl;
 
-        cmp++;
-
     }
-    a.sauvegarde();
 
+
+    clear_bitmap(screen);
 }
